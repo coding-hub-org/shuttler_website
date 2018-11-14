7ce45f7c2114d936c79fd700eda74d205043fad6
keenankunzelman <keekunz@gmail.com>
2018-11-14 16:56:19 -0500

    fixed some stuff

--- shuttler/src/App.js
+++ shuttler/src/App.js
@@ -5,6 +5,7 @@ import SideDrawer from "./components/SideDrawer/SideDrawer";
 import Backdrop from "./components/Backdrop/Backdrop";
 import Page1 from "./components/Page1HomeMobile/Page1";
 import AboutSection from "./components/AboutSection/AboutSection"
+import TeamPage from "./components/TeamPage/Team";
 
 class App extends Component {
   state = {
@@ -32,9 +33,10 @@ class App extends Component {
         <Navbar navClickHandler={this.navToggleClickHandler} />
         <SideDrawer show={this.state.SideDrawerOpen} />
         {backdrop}
-        <main style={{ marginTop: "64px" }}>
+        <main style={{ marginTop: "300px" }}>
           <Page1 />
           <AboutSection />
+          <TeamPage />
         </main>
       </div>
     );
