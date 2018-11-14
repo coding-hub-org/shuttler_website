bccbed25b19e1bb1af67d11439e266adfe25cc8b
Keith Prevost <rickyprev@yahoo.com>
2018-11-14 16:48:27 -0500

    Team component w/ changed page1 component

--- shuttler/src/components/Page1HomeMobile/Page1.js
+++ shuttler/src/components/Page1HomeMobile/Page1.js
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Page1.css";
+import ImageFile from "./image.svg";
 
-const homeBackground = props => {
-  return (
-    <div className="container">
-      <div className="containsTitle">
-        <h1>
-          <span className="title">Don't miss</span> the Shuttle ever again
-        </h1>
-      </div>
+const homeBackground=(props)=>{
 
-      <div className="mobileHomePage" />
+return(
+<div className="container" >
+
+
+
+<div className="mobileHomePage">
+    <div className="containsTitle">
+
+
+    <h1><span className="title">Don't miss</span> the Shuttle ever again</h1>
+
     </div>
-  );
+    <div className="homePageImg">
+        <img src={ImageFile} alt=""/>
+    </div>
+
+</div>
+
+</div>
+)
+
 };
 
 export default homeBackground;
