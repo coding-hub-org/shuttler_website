ef4e5f93c9a1150ba5d61fb3768860a23f65dde2
Gaurav Jayasawal <jgaurav6@gmail.com>
2019-02-08 03:48:23 -0500

    contact form refactored

--- shuttler/src/Sections/SectionContact/Contact.js
+++ shuttler/src/Sections/SectionContact/Contact.js
@@ -5,6 +5,7 @@ import Email from "../../assets/email.png";
 import Phone from "../../assets/phone.png";
 import firebase from "firebase";
 import ScrollableAnchor, { configureAnchors } from "react-scrollable-anchor";
+import SendButton from "../../components/SendButton/SendButton";
 
 class contact extends React.Component {
   state = {
@@ -58,47 +59,49 @@ class contact extends React.Component {
     return (
       <ScrollableAnchor id={"Contact"}>
         <div className="Contact-component">
-        
           <h1 className="contact-header">CONTACT US</h1>
+          <div className="contact-body">
+            <div className="contact-description">
+              <h3>
+                IF YOU HAVE ANY COMMENTS OR SUGESTIONS PLEASE CALL US OR SEND US
+                AN EMAIL
+              </h3>
 
-          <h3>
-            IF YOU HAVE ANY COMMENTS OR SUGESTIONS PLEASE CALL US OR SEND US AN
-            EMAIL
-          </h3>
-          <div className="phone-sec" id="phoneNumDiv">
-            <img className="phone" src={Phone} alt="" />
+              <div className="phone-sec" id="phoneNumDiv">
+                <img className="phone-image" src={Phone} alt="" />
+                <p className="labels">(845)-464-3900</p>
+              </div>
 
-            <p className="labels">(845)-464-3900</p>
-          </div>
-          <div className="phone-sec">
-            <img className="email" src={Email} alt="" />
+              <div className="email-sec">
+                <img className="email-image" src={Email} alt="" />
+                <p className="labels">psucoders@plattsburgh.edu</p>
+              </div>
+            </div>
 
-            <p className="labels">psucoders@plattsburgh.edu</p>
-          </div>
-          <div className="phone-sec">
-            <form action="" id="contactForm">
-              <p className="email-head">Your Email</p>
-              <input
-                onChange={this.handleChangeEmail}
-                className="mail"
-                type="text"
-                id="email"
-              />{" "}
-              <br /> <br />
-              <p className="email-head">Message</p>
-              <textarea
-                onChange={this.handleChangeMessage}
-                className="message"
-                type="text"
-                id="message"
-              />
-              <br />
-              <i
-                onClick={this.handleSubmit}
-                class="far fa-envelope"
-                id="icon"
-              />
-            </form>
+            <div className="contact-form">
+              <form action="" id="contactForm">
+                <p className="email-head">Email address</p>
+                <input
+                  onChange={this.handleChangeEmail}
+                  className="email-input"
+                  placeholder="Email Address"
+                  type="text"
+                  id="email"
+                />{" "}
+                <br /> <br />
+                <p className="message-head">Request</p>
+                <textarea
+                  onChange={this.handleChangeMessage}
+                  className="message-input"
+                  type="text"
+                  placeholder="Type your request..."
+                  id="message"
+                />
+                <div className="sendButton-contactform">
+                  <SendButton />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </ScrollableAnchor>
---
+++ shuttler/src/components/SendButton/SendButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SendButton.css";
+
+const SendButton = () => {
+  return (
+    <div className="SendButton-component">
+      <div>SEND</div>
+    </div>
+  );
+};
+
+export default SendButton;
