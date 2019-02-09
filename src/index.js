322471ff31eaa223f4be662db1a544257768183f
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-02-09 13:46:39 -0500

    Reset content

--- src/index.js
+++ src/index.js
@@ -1,46 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-const Main = () => <h1>Main!</h1>;
-const Elsewhere = () => <h1>Elsewhere!</h1>;
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Main</Link>
-            </li>
-            <li>
-              <Link to="/else">Else</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/else/" component={Elsewhere} />
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
