8c590d71947de168b5bcb0420b4276afaf9e4988
Gaurav Jayasawal <jgaurav6@gmail.com>
2019-05-14 01:39:40 -0400

    Basic validation added

--- src/Sections/Admin/Admin.js
+++ src/Sections/Admin/Admin.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import "./Admin.css";
 import SendButton from "../../components/SendButton/SendButton";
-import Firebase from "../../components/Firebase/firebase";
-import "firebase/firestore";
+import { sendNotification } from "../../components/Firebase/firebase";
 import Title from "../../components/Title/Title";
 import NotificationImage from "../../assets/notificatio_image.png";
 
@@ -27,36 +26,25 @@ class Admin extends Component {
   };
 
   handleSubmit = e => {
-    const db = Firebase.firestore();
-    // Add a new document in collection "cities"
-    db.collection("notifications")
-      .add({
-        date: Date(),
-        title: this.state.title,
-        description: this.state.message
-      })
-      .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-
-        this.setState({
-          title: "",
-          message: "",
-          date: "",
-          isStopped: true,
-          isPaused: true
-        });
-      })
-
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
+    if (this.state.title === "" || this.state.message === "") {
+      alert("Cant be empty");
+    } else {
+      sendNotification(this.state.title, this.state.message);
+      this.setState({
+        title: "",
+        message: "",
+        date: "",
+        isStopped: true,
+        isPaused: true
       });
+    }
   };
 
   render() {
     const defaultOptions = {
       autoplay: false,
       loop: true,
-      animationData: require("../../assets/send_button.json"),
+      animationData: require("../../assets/send_animation.json"),
       rendererSettings: {
         preserveAspectRatio: "xMidYMid slice"
       }
@@ -84,23 +72,25 @@ class Admin extends Component {
               Please enter all details and click on send
             </div>
             <div className="admin-section_form--title">Title</div>
-            <div className="admin-section_form--title_input">
-              <input
-                type="text"
-                name="title"
-                value={this.state.title}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="admin-section_form--message">Message</div>
-            <div className="admin-section_form--message_input">
-              <textarea
-                value={this.state.message}
-                name="description"
-                onChange={this.handleChange}
-                type="text"
-              />
-            </div>
+            <form className="admin-section_form--form">
+              <div className="admin-section_form--title_input">
+                <input
+                  type="text"
+                  name="title"
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="admin-section_form--message">Message</div>
+              <div className="admin-section_form--message_input">
+                <textarea
+                  value={this.state.message}
+                  name="description"
+                  onChange={this.handleChange}
+                  type="text"
+                />
+              </div>
+            </form>
             <div className="admin-section_form--send_button">
               <SendButton click={this.handleSubmit} />
               {/* <Lottie
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import Firebase from "firebase";
 
 const config = {
   apiKey: "AIzaSyA3vvAsqGyP_c8RRkBh44JczzO9y1J0mNQ",
@@ -10,6 +10,29 @@ const config = {
   appId: "1:571374342123:web:a036013b6e887b9e"
 };
 
-firebase.initializeApp(config);
+Firebase.initializeApp(config);
 
-export default firebase;
+const getInstance = () => {
+  return Firebase;
+};
+
+export const sendNotification = (title, message) => {
+  getInstance()
+    .firestore()
+    .collection("notifications")
+    .add({
+      date: Date(),
+      title,
+      description: message
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      alert("Message was sent to all users");
+    })
+
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+};
+
+export default getInstance();
