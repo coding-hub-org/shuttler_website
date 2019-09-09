73f02f07005f11336565d13f94a93259b4f9e55c
winkhanh <giakhanhnk2000@gmail.com>
2019-09-09 12:10:42 -0400

    Kl feat admin login (#5)
    * Commit change to merge master
    
    * Fix: fix the input value of password
    
    * Fix: readd yarn.lock
    
    * Remove config.json
    
    * Remove firebase-admin
    
    * Feat add a Section to switch between Admin and AdminLogin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Main from "./Sections/Main/Main";
 import Maps from "./Sections/Maps/Maps";
-import AdminLogin from "./Sections/AdminLogin/AdminLogin";
-
+import AdminRoute from "./Sections/AdminRoute/AdminRoute";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class App extends Component {
@@ -13,7 +12,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Main} />
             <Route path="/maps/" component={Maps} />
-            <Route path="/admin/" component={AdminLogin} />
+            <Route path="/admin/" component={AdminRoute} />
           </Switch>
         </div>
       </BrowserRouter>
